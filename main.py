ff84dc1dec855cc53e3097de5dabe862d9c0d03e
Anton Polyakov <antonpolyakovvv@mail.ru>
2021-03-03 16:38:31 +0300

    Added new method

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 '''Example'''
 
-def plus(digit1, digit2):
+def plus(digit1, digit2): # Метод для сложения
+    return digit1 + digit2
+
+def minus(digit1, digit2): # Метод для вычитания
     return digit1 + digit2
